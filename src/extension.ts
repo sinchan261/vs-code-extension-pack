acb4f19d234be901a17045ab53f9b10f6c5aaf55
sinchan261 <gsaikat719@gmail.com>
2025-08-18 10:27:39 +0530

    add finding variables functions

--- src/extension.ts
+++ src/extension.ts
@@ -46,6 +46,11 @@ export function activate(context: vscode.ExtensionContext) {
           const result = await analyzeFile(message.filename);
           panel.webview.postMessage({ command: 'showResult', text: result });
         }
+
+		  if( message.command === 'analyzeCode'){
+			const result = await analyzeCodeBlock(message.filepath,message.search);
+			panel.webview.postMessage({command:'showResult1',text:result});
+		  }
 				},
 				undefined,
 				context.subscriptions
@@ -56,13 +61,13 @@ export function activate(context: vscode.ExtensionContext) {
 
 
 
-	async function analyzeFile(filename:string): Promise<string>{
+async function analyzeFile(filename:string): Promise<string>{
 		  const workspaceFolders = vscode.workspace.workspaceFolders;
   if (!workspaceFolders){ return "No workspace open!";}
 
   const workspacePath = workspaceFolders[0].uri.fsPath;
   const filePath = path.join(workspacePath, filename);
-  if (!fs.existsSync(filePath)) {
+  if (!fs.existsSync(filePath)){
     return `❌ File not found: ${filename}`;
   }
   const fileContent = fs.readFileSync(filePath, 'utf8');
@@ -90,11 +95,78 @@ Return results in a clear list format.
     `;
 
     const result = await model.generateContent(prompt);
-    return result.response.text();
+  let output =  result.response.text();
+
+   output = output.replace(/[`*]/g,"").trim();
+   return output ; 
+
   } catch (err) {
     return `Error: ${err}`;
   }
 	}
+
+    // async function for checking analyzecode  from exact file
+
+async function analyzeCodeBlock(filename:string,searchTerm:string){
+
+		const workspaceFolders = vscode.workspace.workspaceFolders;
+		if(!workspaceFolders) {return "No Workspace Folders";}
+          
+		const worksspacepath = workspaceFolders[0].uri.fsPath;
+
+		const filePath = path.join(worksspacepath,filename);
+		if( !fs.existsSync(filePath)){ return `File not found:${filename}`;}
+       
+		const code = fs.readFileSync(filePath,'utf8');
+		const genAI = new GoogleGenerativeAI("AIzaSyDdACIU3h59herh6ZjZnNw0oav4xRe8gK8");
+		const model = genAI.getGenerativeModel({model:"gemini-1.5-flash"});
+const prompt = `
+You are a code analyzer.
+Given this file: ${filename}
+
+Code:
+---
+${code}
+---
+
+Task:
+1. Find all occurrences of "${searchTerm}" that are used and  unused.
+2. Consider scope: variables or functions inside another function or class must be treated separately from globals.
+3. For each occurrence, return:
+   - type (variable/function/import)
+   - exact code
+   - start line and end line
+   - parent scope (global or function/class name)
+4. Do NOT include anything that is **not "${searchTerm}"**.
+5. Return only plain text, no markdown or code blocks.
+
+Example output format:
+
+Variable: unusedVar
+Scope: global
+Lines: 2-2
+Code:
+const unusedVar = 859;
+
+Function: unusedVar
+Scope: function someFunction
+Lines: 5-7
+Code:
+const unusedVar = 90;
+
+`;
+
+
+		 const result = await model.generateContent(prompt);
+		 let output = result.response.text();
+		 output = output.replace(/[`*]/g, "").trim();
+       console.log(output);
+		 return output;
+
+
+	}
+
+
 }
 
 // This method is called when your extension is deactivated
@@ -189,10 +261,39 @@ export function getWebviewContent() {
 		<h3>Result:</h3>
 		<pre id="result">Waiting...</pre>
 
+		//searching and fixing bar 
+              <input id ="filepath" placeholder = "Enter file Path relative to wrokspace"/>
+			  <input id ="search" placeholder = "Enter function /class/console.log to search"/>
+			  <button onclick="analyze1()">Analyze</button>
+			  <h3> AI Analysis Result: </h3>
+			  <Pre id= "result1"> Waiting...</pre>
+
 		<script>
 			const vscode = acquireVsCodeApi();
 
-			function analyze() {
+
+			window.addEventListener('message', event => {
+				const message = event.data;
+				console.log(message.text);
+				if (message.command === 'showResult') {
+					document.getElementById('result').textContent = message.text;
+				}
+                     
+				// insert the code analyze result into frontend
+				if( message.command === 'showResult1'){
+				document.getElementById('result1').textContent = message.text
+				}
+			});
+            
+			// analyze function for code checking
+			function analyze1(){
+			const filepath = document.getElementById('filepath').value;
+			const search = document.getElementById('search').value;
+			 vscode.postMessage({ command: 'analyzeCode',filepath,search});
+			}
+
+               // analyze function for file analyze
+		  function analyze() {
 				const filename = document.getElementById('filename').value;
 				vscode.postMessage({ command: 'analyzeFile', filename });
 			}
@@ -201,12 +302,8 @@ export function getWebviewContent() {
 				vscode.postMessage({ command: 'runsHelloWorld' });
 			}
 
-			window.addEventListener('message', event => {
-				const message = event.data;
-				if (message.command === 'showResult') {
-					document.getElementById('result').textContent = message.text;
-				}
-			});
+
+
 		</script>
 	</body>
 	</html>`;
